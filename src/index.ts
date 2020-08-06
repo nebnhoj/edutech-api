e71e7274511cf265a999ee6a1d4de1f861053666
nebnhoj <john@stash.ph>
2020-08-07 00:36:34 +0800

    init project

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+const port = 8080;
+
+
+
+app.get( "/", ( req, res ) => {
+    res.send({"message":"Hello TSC!"} );
+} );
+
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
